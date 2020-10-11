1efadb5a227800500a25c4e48d837a78e5c63bc3
Alinaufalova <65111595+Alinaufalova@users.noreply.github.com>
2020-10-12 00:07:19 +0500

    Delete Multiplication.java

--- assignment4 SDP/src/com/company/Multiplication.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Multiplication implements Expression{
-    @Override
-    public double exec(double firstNumber, double secondNumber) {
-        return (firstNumber * secondNumber);
-    }
-}
