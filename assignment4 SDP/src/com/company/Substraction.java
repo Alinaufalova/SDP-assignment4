3d239f78a919c77631be75aa35128d4b98a9f0d4
Alinaufalova <65111595+Alinaufalova@users.noreply.github.com>
2020-10-12 00:07:43 +0500

    Delete Substraction.java

--- assignment4 SDP/src/com/company/Substraction.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Substraction implements Expression{
-    @Override
-    public double exec(double firstNumber, double secondNumber) {
-        return (firstNumber - secondNumber);
-    }
-}
